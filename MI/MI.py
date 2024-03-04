e211a69eb9bc2132981af65604506a7343dea509
Ron Jokai <ron.jokai@asu.edu>
2024-03-04 02:07:39 -0700

    [MI protocol] Made nr of nodes a cmd-line argument. Made transactions protobuf files (also cmd-line).

--- MI/MI.py
+++ MI/MI.py
@@ -1,3 +1,11 @@
+import sys
+import transactions_pb2
+
+if len(sys.argv) != int(sys.argv[1]) + 2:
+  print("Usage:", sys.argv[0], "TX_FILE0 TX_FILE1 ...")
+  print("The specified nr of nodes does not match the number of provided tx files.")
+  sys.exit(-1)
+
 class CPU:
     def __init__(self, nr):
         self.nr               = nr
@@ -76,33 +84,62 @@ def dataAccess(nodes, CPUIdx, blkAddr, dir):
 
 # Instantiate nodes and directory.
 nodes = []
-nodes.append( CPU(0) )
-nodes.append( CPU(1) )
-nodes.append( CPU(2) )
+for i in range( int(sys.argv[1]) ):
+    nodes.append( CPU(i) )
+
+#nodes.append( CPU(0) )
+#nodes.append( CPU(1) )
+#nodes.append( CPU(2) )
 
 dir = directory()
 
+txs = []
+total_txs = 0
+
+for i in range( int(sys.argv[1]) ):
+    txs.append( transactions_pb2.Transaction_list() )
+
+    with open(sys.argv[i + 2], "rb") as f:
+        txs[i].ParseFromString(f.read())
+
+    total_txs += len(txs[i].transactions)
+    #print(txs[i])
+
+#print(total_txs)
+
+# TODO Go thru all txs and pick the next-highest-tick one.
+# TODO Currently assuming that ticks are uniquely incremented by one across all txs (1 .. total_txs).
+latest_tick = 0
+for i in range(total_txs):
+    for j in range( len(txs) ):
+        for tx in txs[j].transactions:
+            if tx.tick == i + 1:
+                dataAccess(nodes, tx.nodeID, tx.blkAddr, dir)
+                latest_tick = tx.tick
+                print(tx)
+                print(dir.dirty, dir.entries)
+
 # 1) CPU0 RD 0
-dataAccess(nodes, 0, 0, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 0, 0, dir)
+#print(dir.dirty, dir.entries)
 
 # 2) CPU1 WR 2
-dataAccess(nodes, 1, 2, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 1, 2, dir)
+#print(dir.dirty, dir.entries)
 
 # 3) CPU1 RD 1
-dataAccess(nodes, 1, 1, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 1, 1, dir)
+#print(dir.dirty, dir.entries)
 
 # 4) CPU0 RD 1
-dataAccess(nodes, 0, 1, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 0, 1, dir)
+#print(dir.dirty, dir.entries)
 
 # 5) CPU1 WR 1 TODO CPU1 already has blkAddr though in 'I' state. This would need to replace that, needs to be implemented in CPU logic.
-dataAccess(nodes, 1, 1, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 1, 1, dir)
+#print(dir.dirty, dir.entries)
 
 # 6) CPU2 RD 1
-dataAccess(nodes, 2, 1, dir)
-print(dir.dirty, dir.entries)
+#dataAccess(nodes, 2, 1, dir)
+#print(dir.dirty, dir.entries)
 
